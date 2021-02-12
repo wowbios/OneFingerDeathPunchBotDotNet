b5921a701b94517950a9b4b2e0e1dd8bfc14d778
Aleksey <wowbios@yandex.ru>
2021-02-13 01:56:36 +0300

    Better speed

--- OFDPBot/Program.cs
+++ OFDPBot/Program.cs
@@ -34,7 +34,7 @@ namespace OFDPBot
                 bool wasBrawler = false;
                 while (true)
                 {
-                    var timeout = wasBrawler ? rateMs : rateMs + 50;
+                    var timeout = wasBrawler ? rateMs - 50: rateMs + 50;
                     Thread.Sleep(timeout);
 
                     using Bitmap bmp = screen.MakeScreenshot();
